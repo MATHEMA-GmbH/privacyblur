1f5f8cfe54e76b60dee02ff2a04e14e17cf8b267
Pirmin Bahr <pirmin.bahr@mathema.de>
2021-09-23 10:57:48 +0200

    heapsize for macOS Support

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,66 +1,50 @@
 import Cocoa
-import UIApplication
 import Foundation
 import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+    private var HEAP_SIZE_CHANNEL:String = "de.mathema.privacyblur/memory";
+    
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        let controller : FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+        let heapSizeChannel = FlutterMethodChannel.init(name: HEAP_SIZE_CHANNEL, binaryMessenger:  controller.engine.binaryMessenger)
+        heapSizeChannel.setMethodCallHandler({
+          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          guard call.method == "getHeapSize" else {
+            result(FlutterMethodNotImplemented)
+            return
+          }
+          self?.getHeapSize(result: result)
+        })
+    }
+    
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+
         return true
     }
-
-    private var HEAP_SIZE_CHANNEL:String = "de.mathema.privacyblur/memory";
-
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(
-            name: self.HEAP_SIZE_CHANNEL,
-          binaryMessenger: registrar.messenger)
-        let instance = SharedPreferencesPlugin()
-        registrar.addMethodCallDelegate(instance, channel: channel)
-    }
-
-//    override func application(
-//      _ application: UIApplication,
-//      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//    ) -> Bool {
-//        GeneratedPluginRegistrant.register(with: self)
-//
-//
-//
-//        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-//        let heapSizeChannel = FlutterMethodChannel(name: HEAP_SIZE_CHANNEL, binaryMessenger: controller.binaryMessenger)
-//
-//        heapSizeChannel.setMethodCallHandler({
-//          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-//          guard call.method == "getHeapSize" else {
-//            result(FlutterMethodNotImplemented)
-//            return
-//          }
-//          self?.getHeapSize(result: result)
-//        })
-//    }
     
-//    private func getHeapSize(result: FlutterResult) {
-//      let TASK_VM_INFO_COUNT = MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<natural_t>.size
-//
-//      var vmInfo = task_vm_info_data_t()
-//      var vmInfoSize = mach_msg_type_number_t(TASK_VM_INFO_COUNT)
-//
-//      let kern: kern_return_t = withUnsafeMutablePointer(to: &vmInfo) {
-//              $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-//                  task_info(mach_task_self_,
-//                            task_flavor_t(TASK_VM_INFO),
-//                            $0,
-//                            &vmInfoSize)
-//                  }
-//              }
-//
-//      if kern == KERN_SUCCESS {
-//          let usedSize = Int(vmInfo.internal + vmInfo.compressed)
-//          result(usedSize)
-//      } else {
-//          let errorString = String(cString: mach_error_string(kern), encoding: .ascii) ?? "unknown error"
-//          result("Error with task_info():" + errorString)
-//      }
-//    }
+    private func getHeapSize(result: FlutterResult) {
+      let TASK_VM_INFO_COUNT = MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<natural_t>.size
+
+      var vmInfo = task_vm_info_data_t()
+      var vmInfoSize = mach_msg_type_number_t(TASK_VM_INFO_COUNT)
+
+      let kern: kern_return_t = withUnsafeMutablePointer(to: &vmInfo) {
+              $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                  task_info(mach_task_self_,
+                            task_flavor_t(TASK_VM_INFO),
+                            $0,
+                            &vmInfoSize)
+                  }
+              }
+
+      if kern == KERN_SUCCESS {
+          let usedSize = Int(vmInfo.internal + vmInfo.compressed)
+          result(usedSize)
+      } else {
+          let errorString = String(cString: mach_error_string(kern), encoding: .ascii) ?? "unknown error"
+          result("Error with task_info():" + errorString)
+      }
+    }
 }
