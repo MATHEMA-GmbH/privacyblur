a2877e0070254164e99b343a03f4868b941c7a1e
Pirmin Bahr <pirmin.bahr@mathema.de>
2021-09-30 11:16:44 +0200

    remove unused imports

--- lib/src/screens/main/utils/image_picking.dart
+++ lib/src/screens/main/utils/image_picking.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:privacyblur/src/widgets/theme/theme_provider.dart';
