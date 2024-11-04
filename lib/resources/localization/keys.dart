c39f4a592c6bca8099067a210601040187cdfbea
MATHEADS\yruzhytska <yeva.ruzhytska@mathema.de>
2024-11-04 17:44:27 +0100

    add annotations

--- lib/resources/localization/keys.dart
+++ lib/resources/localization/keys.dart
@@ -1,13 +1,13 @@
-//import 'package:flutter_translate_annotations/flutter_translate_annotations.dart';
+import 'package:flutter_translate_annotations/flutter_translate_annotations.dart';
 import 'package:privacyblur/src/constants.dart';
 
 part 'keys.g.dart';
 
-// Generate static translation keys: flutter pub run build_runner build --delete-conflicting-outputs
+//Generate static translation keys: flutter pub run build_runner build --delete-conflicting-outputs
 
-// @TranslateKeysOptions(
-//     path: localizationResourcesPath,
-//     caseStyle: CaseStyle.titleCase,
-//     separator: "_")
+@TranslateKeysOptions(
+    path: localizationResourcesPath,
+    caseStyle: CaseStyle.titleCase,
+    separator: "_")
 class _$Keys // ignore: unused_element
 {}
